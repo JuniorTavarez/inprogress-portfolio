0eca7fe9336f1e4013ddbf3b2e00dc8c53e85a73
JuniorTavarez <Jtavarez8@gmail.com>
2020-12-09 01:45:46 -0500

    updated changes

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
+// Mobile Menu bar
 const menu = document.querySelector('.menu-bar');
 const nav = document.querySelector('.mobile-nav');
-
-console.log(menu);
-console.log(nav);
 
 menu.addEventListener('click', function () {
   nav.classList.toggle('u-display-none');
 });
+
+// Keeps Box same size
+const p = document.querySelectorAll('.card-1 p');
+// console.log(p);
+
+const resizes = function () {
+  let sum = [];
+
+  p.forEach((element) => {
+    sum.push(element.clientHeight);
+  });
+
+  const total = Math.max(...sum);
+  console.log(total);
+  p.forEach((element) => {
+    element.style.height = `${total}px`;
+  });
+};
+
+resizes();
+
+window.addEventListener('resize', () => resizes());
