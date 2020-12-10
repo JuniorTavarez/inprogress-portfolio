fa3b21c4f14e5c221c3cbb3bf49635b49366c523
juniortavarez <jtavarez8@gmail.com>
2020-12-10 17:37:45 -0500

    finishing site

--- script.js
+++ script.js
@@ -6,33 +6,30 @@ menu.addEventListener('click', function () {
   nav.classList.toggle('u-display-none');
 });
 
-// Keeps Box same size
-const p = document.querySelectorAll('.card-1 p');
-// console.log(p);
+// // Keeps Box same size
+const p = document.querySelector('.card-1 p');
 
-const resizes = function () {
-  let sum = [];
+// const resizes = function () {
+//   let sum = [];
 
-  const run = function () {
-    p.forEach((element) => {
-      console.log(element.getBoundingClientRect().height);
-      sum.push(element.getBoundingClientRect().height);
-    });
-  };
-  run();
-  const total = Math.max(...sum);
-  console.log(sum);
-  p.forEach((element) => {
-    element.style.minHeight = `${total}px`;
-  });
-};
-// console.log('hello');
-resizes();
+//   const run = function () {
+//     p.forEach((element) => {
+//       console.log(element.getBoundingClientRect().height);
+//       sum.push(element.getBoundingClientRect().height);
+//     });
+//   };
+//   run();
+//   const total = Math.max(...sum);
+//   console.log(sum);
+//   p.forEach((element) => {
+//     element.style.minHeight = `${total}px`;
+//   });
+// };
+// // console.log('hello');
+// resizes();
 
-// console.log(element);
-window.addEventListener('resize', function () {
-  location.reload();
-  resizes();
-});
-
-// window.addEventListener('click', resizes());
+// // console.log(element);
+// window.addEventListener('resize', function () {
+//   location.reload();
+//   resizes();
+// });
