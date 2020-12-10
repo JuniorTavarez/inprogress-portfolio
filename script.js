b4850f0c975b9e6303fa8e70eb7608ed636f0fdb
JuniorTavarez <Jtavarez8@gmail.com>
2020-12-10 01:11:59 -0500

    finishing responsive design

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ menu.addEventListener('click', function () {
 });
 
 // Keeps Box same size
-const p = document.querySelectorAll('.card-1 p');
-// console.log(p);
+// const p = document.querySelectorAll('.card-1 p');
+// // console.log(p);
 
-const resizes = function () {
-  //   element.style.height = '0px';
-  let sum = [];
-  console.log(sum);
-  //   sum.splice(0, sum.length);
-  const run = function () {
-    p.forEach((element) => {
-      console.log(element);
-      console.log(element.clientHeight + '&&&&');
-      sum.push(element.getBoundingClientRect().height);
-      console.log(element.getBoundingClientRect());
-    });
-  };
-  run();
-  const total = Math.max(...sum);
-  console.log(total + '****');
-  p.forEach((element) => {
-    element.style.height = `${total}px`;
-  });
-};
-resizes();
-window.addEventListener('resize', () => location.reload());
+// const resizes = function () {
+//   //   element.style.height = '0px';
+//   let sum = [];
+//   console.log(sum);
+//   //   sum.splice(0, sum.length);
+//   const run = function () {
+//     p.forEach((element) => {
+//       console.log(element);
+//       console.log(element.clientHeight + '&&&&');
+//       sum.push(element.getBoundingClientRect().height);
+//       console.log(element.getBoundingClientRect());
+//     });
+//   };
+//   run();
+//   const total = Math.max(...sum);
+//   console.log(total + '****');
+//   p.forEach((element) => {
+//     element.style.height = `${total}px`;
+//   });
+// };
+// resizes();
+// window.addEventListener('resize', () => location.reload());
