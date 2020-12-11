05671c2644b2419273a075a6624a9b5957a59716
juniortavarez <jtavarez8@gmail.com>
2020-12-10 20:01:50 -0500

    updated JS

--- script.js
+++ script.js
@@ -46,3 +46,37 @@ for (let i = 0; i < pArray.length; i++) {
   document.querySelector('.testimonial-2').classList.add('u-display-none');
   // document.querySelector('.testimonial-3').classList.add('u-display-none');
 }
+
+const testimonial1 = document.querySelector('.testimonial-1');
+const testimonial2 = document.querySelector('.testimonial-2');
+const testimonial3 = document.querySelector('.testimonial-3');
+
+let i = 0; // Start Point
+const images = []; // Images Array
+const time = 3000; // Time Between Switch
+
+// Image List
+images[0] = 'http://lorempixel.com/400/200/animals';
+images[1] = 'http://lorempixel.com/400/200/sports';
+images[2] = 'http://lorempixel.com/400/200/food';
+images[3] = 'http://lorempixel.com/400/200/people';
+
+// Change Image
+function changeImg() {
+  document.slide.src = images[i];
+
+  // Check If Index Is Under Max
+  if (i < images.length - 1) {
+    // Add 1 to Index
+    i++;
+  } else {
+    // Reset Back To O
+    i = 0;
+  }
+
+  // Run function every x seconds
+  setTimeout('changeImg()', time);
+}
+
+// Run function when page loads
+window.onload = changeImg;
