2d99f78ee48e0c2fb0bb5d771da9b79e8627aa72
JuniorTavarez <Jtavarez8@gmail.com>
2020-12-20 14:13:14 -0500

    adding animations

--- script.js
+++ script.js
@@ -44,3 +44,50 @@ buttons.forEach((button, index) => {
     testimonialFunction();
   });
 });
+
+// // animation kicks in when scroll
+const leftSide = document.querySelector('#who-am-i .wrapper .left-side');
+const rightSide = document.querySelector('#who-am-i .wrapper .right-side');
+
+// My latest work
+const workHeading = document.querySelector('#my-latest-work .heading');
+
+const cards = document.querySelectorAll('.card-1');
+
+window.addEventListener('scroll', checkBoxes);
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 10) * 8;
+
+  const boxTop = leftSide.getBoundingClientRect().top;
+
+  const boxWorkHeading = workHeading.getBoundingClientRect().top;
+
+  // Who AM I
+  if (boxTop < triggerBottom) {
+    leftSide.classList.add('left-side-slide');
+    rightSide.classList.add('right-side-slide');
+  } else {
+    leftSide.classList.remove('left-side-slide');
+
+    rightSide.classList.remove('right-side-slide');
+  }
+  // My latest work H2 and P
+  // if (workHeading.classList.contains('move-up')) {
+  //   console.log('heyyy');
+  // }
+  if (boxWorkHeading < triggerBottom) {
+    workHeading.classList.add('move-up');
+  } else {
+    workHeading.classList.remove('move-up');
+  }
+
+  cards.forEach((card) => {
+    const boxCard = card.getBoundingClientRect().top;
+    if (boxCard < triggerBottom) {
+      card.classList.add('fade');
+    } else {
+      card.classList.remove('fade');
+    }
+  });
+}
